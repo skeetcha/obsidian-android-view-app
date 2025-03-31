8ec607adbf37fe226f93f471ef1b5c737831aa1d
Cass Unterholzner <cityboundforest@gmail.com>
2025-03-30 20:45:09 -0500

    Moving to Go for mobile development

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use eframe::NativeOptions;
-use obsidian_android_view_app::DemoApp;
-
-fn main() -> Result<(), eframe::Error> {
-    let options = NativeOptions::default();
-    DemoApp::run(options)
-}
